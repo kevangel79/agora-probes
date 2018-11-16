9a2661ee3e9ba04598be4c6744ec5c292d9a55e2
Antonis Angelakis <angelakis@grnet.gr>
2018-11-16 18:04:40 +0200

    Test requests with responses library

--- tests/test_checkhealth.py
+++ tests/test_checkhealth.py
@@ -1,17 +1,21 @@
 import unittest
-#from mock import mock
+import responses
 from agora_probes.checkhealth import AgoraHealthCheck
 
 class AgoraHealthCheckTests(unittest.TestCase):
+    DOMAIN = 'https://localhost.com'
 
     def setUp(self):
-        args = ['-D', 'localhost']
+        args = ['-D', self.DOMAIN]
         self.probe = AgoraHealthCheck(args)
 
     def testCreation(self):
-        self.assertEqual(self.probe.args.domain, 'localhost')
+        self.assertEqual(self.probe.args.domain, self.DOMAIN)
 
+    @responses.activate
     def testCheckEndpoint(self):
+        responses.add(responses.GET, self.DOMAIN,
+                      status=200)
         self.probe.check_endpoint()
         self.assertEqual(self.probe.nagios._code, 0)
 
